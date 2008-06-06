fe04449df6f61ff527a1d16ac6a1c62e67a6fc34
msolo <msolo@adc1de22-563f-0410-814e-2b3882bd23e7>
2008-06-06 03:38:55 +0000

    tag spitfire-0.7

---
+++ tags/spitfire-0.7/spitfire/runtime/repeater.py
@@ -0,0 +1,59 @@
+class RepeatTracker(object):
+  def __init__(self):
+    self.repeater_map = {}
+
+  def __setitem__(self, key, value):
+    try:
+      self.repeater_map[key].index = value
+    except KeyError, e:
+      self.repeater_map[key] = Repeater(value)
+
+  def __getitem__(self, key):
+    return self.repeater_map[key]
+
+class Repeater(object):
+  def __init__(self, index=0, item=None, length=None):
+    self.index = index
+    self.item = item
+    self.length = length
+
+  @property
+  def number(self):
+    return self.index + 1
+
+  @property
+  def even(self):
+    return not (self.index % 2)
+
+  @property
+  def odd(self):
+    return (self.index % 2)
+
+  @property
+  def first(self):
+    return (self.index == 0)
+
+  @property
+  def last(self):
+    return (self.index == (self.length - 1))
+
+class RepeatIterator(object):
+  def __init__(self, iterable):
+    self.src_iterable = iterable
+    self.src_iterator = enumerate(iterable)
+    try:
+      self.length = len(iterable)
+    except TypeError:
+      # if the iterable is a generator, then we have no length
+      self.length = None
+
+  def __iter__(self):
+    return self
+
+  def next(self):
+    index, item = self.src_iterator.next()
+    return Repeater(index, item, self.length)
+    
+    
+    
+    
