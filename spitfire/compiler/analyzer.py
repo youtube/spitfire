4042d883af784fdb2fccaeba5a4a2c4fca277e00
Andrew Braunstein <awbraunstein@google.com>
2016-05-04 13:48:58 -0700

    Some more stuff.

--- spitfire/compiler/analyzer.py
+++ spitfire/compiler/analyzer.py
@@ -694,6 +694,12 @@ class SemanticAnalyzer(object):
         library_function = None
         skip_filter = self.compiler.get_registry_value(fn.expression.name,
                                                        'skip_filter')
+        if self.compile.registry_contains(fn.expression.name):
+          # If this is a placeholder that is in the function registry,
+          # mark it as used so that in the optimizer stage, we can avoid
+          # importing unused registry values.
+          self.template.used_function_registry_identifiers.add(fname)
+
         if isinstance(fn.expression, ast.PlaceholderNode):
             macro_handler_name = 'macro_function_%s' % fn.expression.name
             macro_data = self.compiler.macro_registry.get(macro_handler_name)
